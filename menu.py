f105e04f0739449614c25c9da3b6ca3ee8175242
Thiago Dalamaria <231593048+ThiagoDalamaria@users.noreply.github.com>
2025-10-21 23:14:10 -0300

    Add product registration menu in menu.py

---
+++ menu.py
@@ -0,0 +1,11 @@
+from funções import linha_l
+linha_l()
+print("Cadastro de Produto".center(60))
+linha_l()
+
+
+
+print("1️⃣  -  Cadastrar Produto ")
+print("2️⃣  -  Remover Produto ")
+print("3️⃣  -  Lista cadastrada ")
+print("4️⃣  -  Encerrar programa ")
